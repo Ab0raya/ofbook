40b70ae3ff31d71c56912aaee901faa3a18597c4
Aburayas <tazshakas@gmail.com>
2024-02-23 14:18:58 +0200

    newest get service

--- lib/features/home_feature/data/repos/home_repo.dart
+++ lib/features/home_feature/data/repos/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:ofbook/core/exceptions/failure.dart';
 import 'package:ofbook/features/home_feature/data/models/book_model/book_model.dart';
 
 abstract class HomeRepo{
-  Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure,List<BookModel>>> fetchNewestBooks();
   Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
 }

d1571f16606fa24d671b843b4070adbb692a9047
Aburayas <tazshakas@gmail.com>
2024-02-23 15:34:57 +0200

    add featured cubit method

--- lib/features/home_feature/presentation/manager/featured_list_cubit/featured_list_cubit.dart
+++ lib/features/home_feature/presentation/manager/featured_list_cubit/featured_list_cubit.dart
@@ -1,10 +1,22 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+import 'package:ofbook/features/home_feature/data/repos/home_repo.dart';
 
 import '../../../data/models/book_model/book_model.dart';
 
 part 'featured_list_state.dart';
 
 class FeaturedListCubit extends Cubit<FeaturedListState> {
-  FeaturedListCubit() : super(FeaturedListInitial());
+  FeaturedListCubit(this.homeRepo) : super(FeaturedListInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedListLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(FeaturedListFailure(failure.errMessage));
+    }, (books) {
+      emit(FeaturedListSucceed(books));
+    });
+  }
 }
